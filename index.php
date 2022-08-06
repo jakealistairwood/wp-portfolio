2da21c097d2dbdcbf89378d9742849c3a4e6f8fa
Jake Wood <jakealistairwood@gmail.com>
2022-08-06 11:51:47 +0100

    setup base files to create new wp-theme

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My website</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h2>Content Title</h2>
+    <p>I just built my very first WordPress theme from scratch!</p>
+</body>
+</html>
